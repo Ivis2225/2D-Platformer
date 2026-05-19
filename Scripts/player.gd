9f3154e7237a86fde5502eff57e0997afbdd62d6
Ivis2225 <ivpena@s.sfusd.edu>
2026-05-19 12:23:07 -0700

    Shade Pt 5

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 signal OnUpdateHealth (health : int)
 signal OnUpdateScore (score : int)
 
-
 @export var move_speed :float = 100
 @export var acceleration :float = 50
 @export var braking :float = 20
@@ -11,48 +10,55 @@ signal OnUpdateScore (score : int)
 @export var jump_force :float = 200
 
 var move_input : float
-
+var has_doubled_jumped : bool = false
 @export var health: int = 3
 
 @onready var sprite : Sprite2D = $Character0000
 @onready var anim : AnimationPlayer = $AnimationPlayer
 @onready var audio :AudioStreamPlayer = $AudioStreamPlayer
 
-var take_damage_sfx : AudioStream =preload("res://Audio/take_damage.wav")
+var take_damage_sfx : AudioStream = preload("res://Audio/take_damage.wav")
 var coin_sfx :AudioStream = preload("res://Audio/myinstants-2.mp3")
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		has_doubled_jumped = false
 		
 	move_input = Input.get_axis("move_left", "move_right")
 	
-	if move_input !=0:
-		velocity.x=lerp(velocity.x, move_input * move_speed, acceleration * delta)
-		
+	if move_input != 0:
+		velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, braking * delta)
 	
-	
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = -jump_force
-	
+	if Input.is_action_just_pressed("jump"): 
+		if is_on_floor():
+			velocity.y = -jump_force
+		elif not has_doubled_jumped:
+			velocity.y = -jump_force
+			has_doubled_jumped = true
+
 	move_and_slide()
 
 @warning_ignore("unused_parameter")
 func _process(delta):
-	if velocity.x !=0:
+	if velocity.x != 0:
 		sprite.flip_h = velocity.x > 0
 		
+	# Fall death logic safe deferred check
 	if global_position.y > 200:
-		game_over()
+		health = 0
+		get_tree().call_deferred("change_scene_to_file", "res://Scenes/GameOver.tscn")
 		
 	_manage_animation()
 
 func _manage_animation ():
 	if not is_on_floor():
 		anim.play("Jump")
-	elif move_input !=0:
-			anim.play("Move")
+	elif move_input != 0:
+		anim.play("Move")
 	else:
 		anim.play("idle")
 	
@@ -63,10 +69,12 @@ func take_damage (amount: int):
 	play_sound(take_damage_sfx)
 	
 	if health <= 0:
-		call_deferred("game_over")
+		# Godot 4 safe deferred scene change syntax
+		get_tree().call_deferred("change_scene_to_file", "res://Scenes/GameOver.tscn")
 		
 func game_over():
-	get_tree().change_scene_to_file("res://Scenes/end_game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/GameOver.tscn")
+
 func increase_score (amount : int):
 	PlayerStats.score += amount
 	OnUpdateScore.emit(PlayerStats.score)
@@ -77,8 +85,10 @@ func _damage_flash ():
 	await get_tree().create_timer(0.5).timeout
 	sprite.modulate = Color.WHITE
 
-
 func play_sound (sound : AudioStream):
 	audio.stream = sound
 	audio.play()
-	
+
+func _on_water_body_entered(body: Node2D) -> void:
+	health = 0
+	get_tree().call_deferred("change_scene_to_file", "res://Scenes/end_game.tscn")
